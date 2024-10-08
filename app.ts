67b238c389c6544add7373eb0c2e0aee747e00be
lazycoder54 <pranjitd@gmail.com>
2024-10-08 20:25:29 +0530

    cors

--- app.ts
+++ app.ts
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["https://e-learning-client-sigma.vercel.app"],
+    origin: ["https://e-learning-client-sigma.vercel.app","http://localhost:3000"],
     credentials: true,
   })
 );
